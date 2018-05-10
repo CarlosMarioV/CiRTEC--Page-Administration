4a98a6713950bf042ba689c48ed8c75274f5bb03
EstebanBlanco <eblanco707@gmail.com>
2018-05-09 21:47:57 -0600

    Modulo de Login y Single Project

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ var miapp = angular.module('CiRTEC', ['ui.router']);
 miapp.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
-  $stateProvider.state('landing_page', {
-    url: '/',
-    templateUrl: './Landing_page/landingPage.html',
-    controller: 'LandingPageCtrl',
-    controllerAs: 'home'
-  });
+  $stateProvider
+  .state(
+        'landing_page', {
+        url: '/',
+        templateUrl: './Landing_page/landingPage.html',
+        controller: 'LandingPageCtrl',
+        controllerAs: 'home'
+    })
+  .state(
+        'login', {
+        url: '/login',
+        templateUrl: './login/login.html',
+        controller: 'LoginCtrl',
+        controllerAs: 'login'
+  })
 });
